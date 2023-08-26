58794df550b7a5948217c3186dce141e8f58316e
Rudra <rudrasnarola2@gmail.com>
2023-08-26 10:26:23 +0530

    Change Sign In to Log In

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -3,7 +3,7 @@ export default class Login extends Component {
   render() {
     return (
       <form>
-        <h3>Sign In</h3>
+        <h3>Log In</h3>
         <div className="mb-3">
           <label>Email address</label>
           <input
